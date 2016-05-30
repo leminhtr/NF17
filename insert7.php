d7bd6e602dd1c85133fd91784f9128141fc348d3
Mamal <eumael.desseaux@etu.utc.fr>
2016-05-30 12:04:05 +0200

    Modification des fichier insert 5-6-7 (il ne marchent pas encore)

---
+++ insert7.php
@@ -0,0 +1,52 @@
+<html>
+<?php
+include 'mise_en_page.php';
+?>
+<h1>Etape 4 de la création de votre CV : Formation</h1>
+  <p>Veuillez remplir les champs ci-dessous</p>
+<?php
+/*	include 'connect_projet.php';
+	$vCompt = $_POST['Compt'];
+	$vLang = $_POST['Langue'];
+	$vCon = fConnect();
+
+	$vSQL = 'SELECT nom, langue FROM Competences WHERE nom = $vCompt AND langue = $vLang';
+	if(pg_query($vCon,$vSQL) == NULL){
+		$vSQL = 'INSERT INTO Competences(nom,langue) VALUES ($vCompt,$vLang)';
+		pg_query($vCon,$vSQL);
+	}
+*/
+	
+	//-> récuperer l'id du candidat !!!
+
+/*	$vID = ....
+	
+	$vSQL = 'SELECT id, nom, langue FROM Posseder_Competence WHERE nom = $vCompt AND langue = $vLang';
+	if(pg_query($vCon,$vSQL) == NULL){
+		$vSQL = 'INSERT INTO Posseder_Competence(id_candidat,nom,langue) VALUES ($vID,$vCompt,$vLang)';
+		pg_query($vCon,$vSQL);
+	}
+
+*/
+?>
+  <form method= "post" action="insert8.php">
+	<label>Domaine d'étude :<label>
+	</br>
+	<label>Nom français<label> : <input type="text" name="DE_FR" />
+	</br>
+	<label>Nom anglais<label> : <input type="text" name="DE_EN" />
+	</br>
+	<label>Date de début<label> : <input type="date" name="date_debut" />
+	</br>
+	<label>Date de fin<label> : <input type="date" name="date_fin" />
+	</br>
+	<label>Entablissement<label> : <input type="text" name="Etab" />
+	</br>
+	<label>Pays<label> : <input type="text" name="pays" />
+	</br>
+	<label>Ville<label> : <input type="text" name="ville" />
+	</br>
+	<input type="submit" value="Ajouter une autre formation" />
+	<input type="submit" value="Terminer"/>
+    	</p>
+  </form>
