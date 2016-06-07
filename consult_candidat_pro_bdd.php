39b9bce5daa1702d81a13ace980bb8d7b7df1f7e
Minh Tri Lê <minh-tri.le@etu.utc.fr>
2016-06-07 21:10:44 +0200

    suite

--- consult_candidat_pro_bdd.php
+++ consult_candidat_pro_bdd.php
@@ -1,47 +1,8 @@
 <!DOCTYPE html>
 <html>
-<head>
-    <meta charset="UTF-8">
-    <title>Consulter les candidats ayant comme compétence "base de donnée" et plus de 5 ans d'expériences</title>
-</head>
-<body>
-
-<div id="contenu">
-    <div id="Menu">
-    </div>
-    <div id="Bienvenue">
-    </div>
-</div>
-
-<nav>
-    <ul>
-        <li><a href="page1_projet.php">Accueil</a></li>
-        </br>
-        <p>Espace Candidats</p>
-        </br>
-        <li><a href="insert.php">Ajouter votre CV</a></li>
-        <li><a href="check.php">Modifier votre CV</a></li>
-        </br>
-        <p>Espace Entreprise</p>
-        </br>
-        <li><a href="select.html">Consulter les CV</a></li>
-        <br>
-        <p>Espace Référents</p>
-        </br>
-        <li><a href="consult_referent.html">Consulter vos candidats</a></li>
-    </ul>
-</nav>
-
-<style>
-    nav{
-        float:left;
-        width:25%;
-        height:100%;
-        border-right:1px dashed #CCC;
-        /*padding:20px;
-        margin-top:40px;*/
-    }
-</style>
+<?php
+include 'mise_en_page.html';
+?>
 
 <h1>Consulter les candidats ayant comme compétence "base de donnée" et plus de 5 ans d'expériences</h1>
 
---
+++ insert7.php
@@ -0,0 +1,45 @@
+<?php session_start();?>
+<html>
+<?php
+include 'mise_en_page.html';
+?>
+<h1>Etape 4 de la création de votre CV : Formation</h1>
+  <p>Veuillez remplir les champs ci-dessous</p>
+<?php
+	include 'connect_projet.php';
+	$vCompt = $_POST['Compt'];
+	$vLang = $_POST['Langue'];
+	$vCon = fConnect();
+
+	$vSQL = "SELECT nom, langue FROM Competences WHERE nom = $vCompt AND langue = $vLang";
+	$vQuery = pg_query($vCon,$vSQL);
+	if(pg_fetch_query($vQuery, NULL, PGSQL_ASSOC) == NULL){
+		$vSQL = "INSERT INTO Competences(nom,langue) VALUES ($vCompt,$vLang)";
+		pg_query($vCon,$vSQL);
+	}
+
+	
+	//-> récuperer l'id du candidat !!!
+
+	$vID = $_SESSION['id'];
+	
+	$vSQL = "SELECT id, nom, langue FROM Posseder_Competence WHERE nom = $vCompt AND langue = $vLang";
+	$vQuery = pg_query($vCon,$vSQL);
+	if(pg_fetch_query($vQuery, NULL, PGSQL_ASSOC) == NULL){
+		$vSQL = "INSERT INTO Posseder_Competence(id_candidat,nom,langue) VALUES ($vID,$vCompt,$vLang)";
+		pg_query($vCon,$vSQL);
+	}
+
+
+	echo "La formation que vous voulez insérer est elle traduite ?";
+	echo '<p><a href="insert8.php">Oui</a></p>';
+	echo '<p><a href="insert9.php">Non</a></p>';
+
+?>
+
+
+?>
+
+<hr/>
+</body>
+</html>
